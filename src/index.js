ece18513d819ceb591cb19212ffecd112d58e6e8
Vlad <71841810+TheHumankind@users.noreply.github.com>
2021-02-20 03:59:10 +0300

    All tests p

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+    let newN = 0;
+    n = Math.abs(n);
+    while(n >= 10){
+        newN = newN + n % 10;
+        n /= 10;
+        n = Math.floor(n);
+        newN = newN * 10;
+    }
+    
+    return newN + n;
 }
